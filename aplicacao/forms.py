265f0d4cddbc1ef4246655625eeffb15fb6c39a8
Caetano Smaniotto <smaniottocaetano@gmail.com>
2024-03-29 11:05:17 -0300

    0.1
    css
    js

--- aplicacao/forms.py
+++ aplicacao/forms.py
@@ -6,17 +6,39 @@ from aplicacao.models import Funcionario
 from datetime import datetime
 
 
+class FormCriarConta(FlaskForm):
+    nome = StringField('Nome: ', validators=[DataRequired(), Length(max=75)])
+    email = StringField("Email", validators=[DataRequired(), Length(max=250), Regexp(r'^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$', message="E-mail inválido")])
+    telefone = StringField('Telefone: ',  validators=[DataRequired()])
+    senha = PasswordField('Senha: ', validators=[DataRequired()])
+    confirmacao_senha = PasswordField("Confirme sua senha", validators=[DataRequired(), EqualTo("senha", message="Senhas não coincidem!")])
+    botao_enviar = SubmitField('Criar Conta')
+
+    def validate_email(self, email): 
+        email = Usuario.query.filter_by(email=email.data).first()
+        if email:
+            raise ValidationError("Erro! E-mail já utilizado!")
+        
+    def validate_senha(self, senha):
+        if len(senha.data) < 8:
+            raise ValidationError("A senha deve ter mais de 8 caracteres")
+        
+    def validate_senha(self, telefone):
+        telefone = Usuario.query.filter_by(telefone=telefone.data).first()
+        if telefone:
+            raise ValidationError("Erro! Número de telefone já cadastrado")
+
 funcionarios = Funcionario.query.all()
 
 class FormBuscarHorarios(FlaskForm):
     funcionarios_disponiveis = RadioField('Com quem deseja marcar?', choices=[(funcionario.id, funcionario.nome) for funcionario in funcionarios], validators=[DataRequired()])
-    data = DateField('Selecione a data', format='%Y-%m-%d', validators=[DataRequired()])
+    data = DateField('Selecione a data', format='%Y-%m-%d', validators=[DataRequired()], default=datetime.now().date())
     botao_enviar = SubmitField('Buscar Horários')
 
     def validate_data(self, data):
         if data.data < datetime.now().date():
-            flash('A data selecionada não pode ser anterior à data atual.')
-            raise ValidationError('A data selecionada não pode ser anterior à data atual.')
+            flash('Data inválida!')
+            raise ValidationError('Data inválida!')
 
 class FormConfirmarHorario(FlaskForm):
     botao_confirmar = SubmitField('Confirmar horário')
--- aplicacao/static/js/script.js
+++ aplicacao/static/js/script.js
@@ -1,8 +1,52 @@
-const btnMostrar = document.getElementsByClassName("m-mostrar")[0]; // Selecionando o primeiro elemento
-const listaItens = document.getElementsByClassName("itens")[0]; // Selecionando o primeiro elemento
-const btnSeta = document.getElementsByClassName("rotate")[0];
+// timer do alerta
+setTimeout(function () {
+    document.getElementById("alerta").style.visibility = "hidden";
+}, 4000);
 
-btnMostrar.addEventListener('click', () => {
-    listaItens.classList.toggle('v');
-    btnSeta.classList.toggle('rotate');
+setTimeout(function(){ 
+    document.getElementById("alerta").style.opacity='0';
+}, 1000);
+
+
+// opções de acessibilidade
+const changeSize = () => {
+    const elements = document.querySelectorAll('*');
+
+    elements.forEach(element => {
+        element.classList.toggle('changeFont');
+    });
+
+    // Verifica se a classe de fonte foi ativada e armazena o estado no localStorage
+    const isFontChanged = elements[0].classList.contains('changeFont');
+    localStorage.setItem('fontChanged', isFontChanged);
+
+    const btnSize = document.getElementById('btnSize');
+
+    btnSize.classList.toggle('active');
+}
+
+function toggleDarkMode() {
+    var element = document.body;
+    element.classList.toggle("dark"); 
+    
+    const darkMode = element.classList.contains('dark');
+    localStorage.setItem('darkMode', darkMode);
+
+    const btnContraste = document.getElementById('btnContraste');
+    btnContraste.classList.toggle('active');
+}
+
+// Obtém o estado da classe de fonte do localStorage ao carregar a página
+document.addEventListener('DOMContentLoaded', () => {
+    const fontChanged = localStorage.getItem('fontChanged');
+    const darkMode = localStorage.getItem('darkMode');
+
+    if (fontChanged === 'true') {
+        changeSize();
+    }
+
+    if (darkMode === 'true') {
+        toggleDarkMode()
+    }
+
 });
